2844032e3d535100515b7e8d940db77db5c8820b
birdsoftheworld <exoticbirdsoftheworld@gmail.com>
2022-03-14 23:31:35 -0700

    Add ECS

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 pub mod parser;
 
+mod english {
+    pub const PREFIXES: [&str; 4] = ["the", "an", "a", "some"];
+}
+
 pub mod item {
+    use std::any::{Any, TypeId};
+    use std::collections::HashMap;
+    use crate::english::PREFIXES;
+
     #[derive(Debug, Clone)]
     pub struct ItemString(Vec<String>);
-
-    const PREFIXES: [&str; 4] = ["the", "an", "a", "some"];
     
     impl ItemString {
         pub fn new(mut data: Vec<String>) -> ItemString {
@@ -17,6 +23,28 @@ pub mod item {
         pub fn from_refs(data: Vec<&str>) -> ItemString {
             ItemString::new(data.iter().map(|slice| (*slice).to_owned()).collect())
         }
+    }
+
+    pub struct Entity {
+        components: HashMap<TypeId, Box<dyn Component>>
+    }
+
+    impl Entity {
+        fn add_component(&mut self, component: Box<dyn Component>) -> bool {
+            self.components.insert(component.type_id(), component).is_some()
+        }
+
+        fn remove_component(&mut self, component: Box<dyn Component>) -> bool {
+            self.components.remove(&component.type_id()).is_some()
+        }
+
+        fn get_component(&mut self, component: Box<dyn Component>) -> Option<&Box<dyn Component>> {
+            self.components.get(&component.type_id())
+        }
+    }
+
+    pub trait Component {
+
     }
 }
 
